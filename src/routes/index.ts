64b126cf6362b6d4a58346b6e1f1d41ec1458b8e
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2025-12-16 18:11:57 -0300

    feat: adding user swagger annotation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import userRoutes  from "@routes/userRouter"
 import authenticationRoutes from "@routes/authenticationRouter"
 import { authMiddleware } from "middleware/authMiddleware"
 import { Knex } from "knex";
+import docRoutes from "@routes/docRouter";
 
 
 const registerRouter = (db: Knex) => {
@@ -11,7 +12,12 @@ const registerRouter = (db: Knex) => {
   const API_VERSION = process.env.API_VERSION || "v1"
 
   router.use(`/${API_VERSION}`,authenticationRoutes(db)) //public routes
+  router.use(`/${API_VERSION}`,docRoutes())
+
+
   router.use(`/${API_VERSION}`,authMiddleware,userRoutes(db)) //auth routes
+
+
 
   return router
 }
