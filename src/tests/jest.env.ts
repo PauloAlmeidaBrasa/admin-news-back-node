28c8446eb5b5e4e51dad8614600ab3311b1d294e
almeida.desenvolve@gmail.com <almeida.desenvolve@gmail.com>
2025-12-23 12:00:39 -0300

    fix: adding missing env variable

--- src/tests/jest.env.ts
+++ src/tests/jest.env.ts
@@ -3,4 +3,4 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.test" });
 
 console.log("🧪 Running tests with DB:", process.env.DB_NAME);
-console.log("🧪 Running USER DB:",process.env.DB_USER)
+// console.log("🧪 Running USER DB:",process.env.DB_USER)
